1f41571d226cf8dc016cafe9bc12850c16ab9a9b
Lucas544875 <takayanagi691@gmail.com>
2024-05-05 22:37:44 +0900

    [add] reverseを追加

--- ids-tree.py
+++ ids-tree.py
@@ -2,11 +2,14 @@ import json
 import re
 
 inputfile = "data/UCSkanji-smashed.json"
-outputfile = "data/UCSkanji-tree.json"
+# outputfile = "data/UCSkanji-tree.json"
+outputfile = "data/UCSkanji-tree-reverse.json"
 # inputfile = "data/joyo-smashed.json"
 # outputfile = "data/joyo-tree.json"
+# outputfile = "data/joyo-tree-reverse.json"
 # inputfile = "data/test.json"
 # outputfile = "data/test-tree.json"
+# outputfile = "data/test-tree-reverse.json"
 
 data = {}
 with open(inputfile, mode="r", encoding="utf-8") as f:
@@ -19,12 +22,13 @@ tree = {}
 for kanji in data:
     ids = data[kanji]
     ids_ary = ids_split(ids)
+    ids_ary.reverse()
     current_elem = tree
-    for i in range(len(ids_ary)-1):
+    for i in range(len(ids_ary)):
         if ids_ary[i] not in current_elem:
             current_elem[ids_ary[i]] = {}
         current_elem = current_elem[ids_ary[i]]
-    current_elem[ids_ary[-1]] = kanji
+    current_elem["terminal"] = kanji
 
 with open(outputfile, mode="w", encoding="utf-8") as g:
     g.write(json.dumps(tree))
